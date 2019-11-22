b910a0283d927d45a3b3e1e6b9e0ee7f9fb22691
GodlewskiKonrad <31934@student.pwsztar.edu.p>
2019-11-22 09:19:33 +0100

    I tak nie dziala :(

---
+++ navbar.js
@@ -0,0 +1,14 @@
+let nav = '<h1>Nawigacja</h1>'+
+            '<ul class="nav">'+
+              '<li class="nav-item">'+
+                  '<a class="nav-link active" href="index.html">Index</a>'+
+              '</li>'+
+              '<li class="nav-item">'+
+                  '<a class="nav-link" href="contact.html">Kontakt</a>'+
+              '</li>'+
+          '</ul>';
+
+let elem = document.querySelector('.navbar');
+elem.innerHtml = nav;
+
+
